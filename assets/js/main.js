00ca6aaf7dda5292b8223ef67c38b6f6896cc16c
Gozzal <sadirbaevagozzal@gmail.com>
2023-01-03 22:02:55 +0500

    ignore

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+function changeParticle(){
+    var navbarcolor = document.getElementById('navbarcolor');
+    var scollValeu = window.scrillY;
+    if(scollValeu < 50){
+        navbarcolor.classList.remove(navbarccolor);
+    } else{
+        navbarcolor.classList.add()
+    }
+}
